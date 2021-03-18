9c648757bee885ce853c92109554215aea19b284
Fabian Sauter <sauter.fabian@mailbox.org>
2021-03-18 12:53:24 +0100

    Writing before reading

--- src/jutta_snooper/JuttaSnooper.cpp
+++ src/jutta_snooper/JuttaSnooper.cpp
@@ -9,13 +9,18 @@ namespace jutta_snooper {
 JuttaSnooper::JuttaSnooper(const std::string& device) : connection(device) {}
 
 void JuttaSnooper::run() {
-    std::vector<uint8_t> buffer{};
+    std::vector<uint8_t> readBuffer{};
+    std::vector<uint8_t> writeBuffer{'H', 'a', 'l', 'l', 'o', '\r', '\n'};
     while (true) {
-        connection.read_decoded(buffer);
-        if (!buffer.empty()) {
-            std::cout << "Read " << buffer.size() << " bytes\n";
-            jutta_proto::JuttaConnection::print_bytes(buffer);
-            buffer.clear();
+        // Write test data:
+        connection.write_decoded(writeBuffer);
+
+        // Read test data:
+        connection.read_decoded(readBuffer);
+        if (!readBuffer.empty()) {
+            std::cout << "Read " << readBuffer.size() << " bytes\n";
+            // jutta_proto::JuttaConnection::print_bytes(buffer);
+            readBuffer.clear();
         }
     }
 }
