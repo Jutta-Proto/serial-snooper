d2020a5903f25d2d936d99718d5f9c3e29245a28
Fabian Sauter <sauter.fabian@mailbox.org>
2021-03-18 11:29:31 +0100

    Added basic snooping logic

--- src/jutta_snooper/JuttaSnooper.cpp
+++ src/jutta_snooper/JuttaSnooper.cpp
@@ -1,4 +1,7 @@
 #include "JuttaSnooper.hpp"
+#include "jutta_proto/JuttaConnection.hpp"
+#include <iostream>
+#include <vector>
 
 //---------------------------------------------------------------------------
 namespace jutta_snooper {
@@ -6,6 +9,15 @@ namespace jutta_snooper {
 JuttaSnooper::JuttaSnooper(const std::string& device) : connection(device) {}
 
 void JuttaSnooper::run() {
+    std::vector<uint8_t> buffer{};
+    while (true) {
+        connection.read_decoded(buffer);
+        if (!buffer.empty()) {
+            std::cout << "Read " << buffer.size() << " bytes\n";
+            jutta_proto::JuttaConnection::print_bytes(buffer);
+            buffer.clear();
+        }
+    }
 }
 //---------------------------------------------------------------------------
 }  // namespace jutta_snooper
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include "jutta_snooper/JuttaSnooper.hpp"
 #include "logger/Logger.hpp"
 
 int main(int /**argc**/, char** /**argv**/) {
     logger::setup_logger(spdlog::level::debug);
     SPDLOG_INFO("Launching Version: {} {}", JUTTA_SNOOPER_VERSION, JUTTA_SNOOPER_VERSION_NAME);
+
+    jutta_snooper::JuttaSnooper snooper("/dev/ttyS1");
+    snooper.run();
     return 0;
 }
