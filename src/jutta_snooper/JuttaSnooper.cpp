824cfc494cfc7d7161550d6e2f67621cceffbf68
Fabian Sauter <sauter.fabian@mailbox.org>
2021-06-02 17:50:58 +0200

    Printing read content

--- src/jutta_snooper/JuttaSnooper.cpp
+++ src/jutta_snooper/JuttaSnooper.cpp
@@ -68,9 +68,9 @@ void JuttaSnooper::uart_run() {
     while (true) {
         connectionLock.lock();
         static_cast<void>(connection.read_decoded(readBuffer));
-        connectionLock.unlock();
         if (!readBuffer.empty()) {
-            SPDLOG_DEBUG("Read {} bytes.", readBuffer.size());
+            std::string resultRead = jutta_proto::JuttaConnection::vec_to_string(readBuffer);
+            SPDLOG_DEBUG("Read {} bytes: {}", readBuffer.size(), resultRead);
             // jutta_proto::JuttaConnection::print_bytes(buffer);
             readBuffer.clear();
         }
